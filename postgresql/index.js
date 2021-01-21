2bc781876f1da7512710ab0dec8d31aa572f4916
David Hardy <david.hardy50@gmail.com>
2021-01-21 15:23:27 +0100

    direct heroku link

--- postgresql/index.js
+++ postgresql/index.js
@@ -10,13 +10,15 @@ const username = process.env.USER;
 const password = process.env.PASSWORD;
 const host = process.env.HOST || 'localhost';
 
-const sequelize = new Sequelize (database, username, password, {
-  host: host,
-  dialect: 'postgres',
-  // dialectOptions: {
-  //   ssl: false
-  // }
-})
+// const sequelize = new Sequelize (database, username, password, {
+//   host: host,
+//   dialect: 'postgres',
+//   // dialectOptions: {
+//   //   ssl: false
+//   // }
+// })
+
+const sequelize = new Sequelize ('postgres://nppakpwlkbbomz:18ef7c34bf2931a50c82cb1521ee0c0ae3eeed24837b57aa6ab9ebdcf22c05ad@ec2-18-203-7-163.eu-west-1.compute.amazonaws.com:5432/d7jj77cc8t1p64')
 
 db.Meals = mealsFactory(sequelize)
 db.Drinks = drinksFactory(sequelize)
